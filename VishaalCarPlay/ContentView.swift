8d77e13e4d57e925e56f827edf3caed8cb2ebddf
Vishaal Prasad <v.prasad2@samsung.com>
2024-03-05 15:41:30 -0800

    Initial Commit

---
+++ VishaalCarPlay/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VishaalCarPlay
+//
+//  Created by Vishaal Prasad on 3/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VishaalCarPlay/VishaalCarPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  VishaalCarPlayApp.swift
+//  VishaalCarPlay
+//
+//  Created by Vishaal Prasad on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VishaalCarPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
